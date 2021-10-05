c7478ee673406d0789cb4a0617e6390145073dd5
Yip Ka Yan <yipkayan3@gmail.com>
2021-10-05 21:06:44 +0800

    added navigation
    /users
    /

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+//app.use(express.static(path.join(__dirname,"public")));
+
 app.get('/',(req,res)=>{
-    res.send("hello world");
+    res.sendFile(path.join(__dirname,"public/home.html"));
 });
 
 app.get('/users',(req,res)=>{
-    res.send("hello users");
+    res.sendFile(path.join(__dirname,"public/users.html"));
 });
 
 app.listen(3000);
